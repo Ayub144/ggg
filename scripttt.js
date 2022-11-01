0d9adcfc48b0a452b6935fe1a71f4fb921b0e1b0
elmurzaev <elmurzaev@gmail.com>
2022-11-01 18:13:59 +0300

    swe commot

---
+++ scripttt.js
@@ -0,0 +1,23 @@
+//Задание 1
+const WS_SIZE = 25;
+if ( WS_SIZE > 10){console.log("константа не меньше 10");}
+else {console.log('конста меньше 10');}
+//Задание 2
+let text = 'значение';
+if (text = 'lorem' ) {console.log("у вас ошибка в тексте");}
+else {console.log ('Содержимое переменной');}
+//Задание 3
+let comment = 'Word Exel';
+if (comment = length){console.log ('Текст слишком длинный.В нем 230 символов.');}
+else {console.log (comment.length);}
+//Задание 4
+let cartSum = 10000;
+let cash = 5000;
+total = (10000 * 0.05);
+if (cartSum >= cash){console.log('ваша скидка ${total} Доставка бесплатно');}
+else (cartSum < cash);
+total = (5000 - 0.05);{console.log('Итого к оплате ${total} до скидки осталось ${cash}');}
+
+
+
+
